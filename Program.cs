8e96b59e0ca5b12b78190562e47761994315ff8e
Hunter O'Toole <90637885+Topzombie22@users.noreply.github.com>
2021-12-11 13:23:26 -0400

    Added Base/Menu/PlayerInput

--- Program.cs
+++ Program.cs
@@ -4,9 +4,120 @@ namespace TurnBasedRPG
 {
     class Program
     {
+        static float maxMonsterHP;
+        static float currentMonsterHP;
+        static float monsterDamage;
+        static float maxPlayerHP;
+        static float currentPlayerHP;
+        static float playerDamage;
+        static float healthPotionHeal;
+        static float blockReduction;
+        static bool onMenu;
+        static bool inGame;
+        static bool gameOver;
+        static bool playerTurn;
+        static bool monsterTurn;
+        static bool shopping;
+        static bool attacking;
+        static bool defending;
+        static bool healing;
+
+        static int playerInGameMenu;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            playerInGameMenu = 1;
+            PlayerTurn();
+            Console.WriteLine("FightDome...");
+            Console.ReadKey();
+        }
+
+        static void PlayerTurn()
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                PlayerMenu();
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.UpArrow && playerInGameMenu != 1)
+                {
+                    playerInGameMenu = playerInGameMenu - 1;
+                }
+                if (cki.Key == ConsoleKey.DownArrow && playerInGameMenu != 5)
+                {
+                    playerInGameMenu = playerInGameMenu + 1;
+                }
+                if (cki.Key == ConsoleKey.Spacebar)
+                {
+                    break;
+                }
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            } while (cki.Key != ConsoleKey.Spacebar || cki.Key != ConsoleKey.Enter);
+        }
+
+        static void PlayerAction()
+        {
+            
+        }
+
+        static void PlayerMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Players Turn");
+            Console.WriteLine("███████████████████████████████████████████████████████████████████████████████████████████████████████████");
+            Console.WriteLine("█                                                                                                         █");
+        if (playerInGameMenu == 1)
+            {
+                Console.WriteLine("█  → Attack                                                                                               █");
+            }
+        if (playerInGameMenu != 1)
+            {
+                Console.WriteLine("█  Attack                                                                                                 █");
+            }
+
+
+            Console.WriteLine("█                                                                                                         █");
+        if (playerInGameMenu == 2)
+            {
+                Console.WriteLine("█  → Block                                                                                                █");
+            }
+        if (playerInGameMenu != 2)
+            {
+                Console.WriteLine("█  Block                                                                                                  █");
+            }
+            Console.WriteLine("█                                                                                                         █");
+        if (playerInGameMenu == 3)
+            {
+                Console.WriteLine("█  →  Heal                                                                                                █");
+            }
+        if (playerInGameMenu != 3)
+            {
+                Console.WriteLine("█  Heal                                                                                                   █");
+            }
+            Console.WriteLine("█                                                                                                         █");
+        if (playerInGameMenu == 4)
+            {
+                Console.WriteLine("█  →  Save Game                                                                                           █");
+            }
+        if (playerInGameMenu != 4)
+            {
+                Console.WriteLine("█  Save Game                                                                                              █");
+            }
+            Console.WriteLine("█                                                                                                         █");
+        if (playerInGameMenu == 5)
+            {
+                Console.WriteLine("█  →  Exit to Menu                                                                                        █");
+            }
+        if (playerInGameMenu != 5)
+            {
+                Console.WriteLine("█  Exit to Menu                                                                                           █");
+            }
+            Console.WriteLine("█                                                                                                         █");
+            Console.WriteLine("███████████████████████████████████████████████████████████████████████████████████████████████████████████");
         }
     }
 }
